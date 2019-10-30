d5106711394b5f4528143b440344118996db1235
Jeremy Schuurmans <schuurm@ns.codes>
2019-10-30 15:55:55 -0700

    complete test for API call

--- spec/reader/get_book_spec.rb
+++ spec/reader/get_book_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe Reader::GetBook do
     it 'can fetch and parse books data from the Google Books API' do
       VCR.use_cassette("reader") do
         response = Net::HTTP.get_response(URI("https://www.googleapis.com/books/v1/volumes?q=ruby&maxResults=5&orderBy=relevance&printType=books&key=#{ENV['GOOGLE_BOOKS_KEY']}"))
+        expect(response.body).to be_a(String) 
+        expect(response.body).to include('title')
+        expect(response.body).to include('authors')
+        expect(response.body).to include('publisher')
+        expect(response.body).not_to include('error: {')
       end
 
 
