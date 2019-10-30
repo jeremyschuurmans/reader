3f79d0868c56bd0e034d68ad80cae28b8b8499f9
Jeremy Schuurmans <schuurm@ns.codes>
2019-10-30 13:29:38 -0700

    create get_book.rb, require it in reader.rb

--- lib/reader.rb
+++ lib/reader.rb
@@ -1,7 +1,8 @@
 require 'pry'
 
-require_relative "reader/version"
-require_relative "reader/book"
+require_relative 'reader/version'
+require_relative 'reader/book'
+require_relative 'reader/get_book'
 
 module Reader
 end
