556af2453118aec33416683e5f45991bfd4f7aed
Jeremy Schuurmans <schuurm@ns.codes>
2019-10-31 10:51:58 -0700

    create reading_list.rb, require in reader.rb, create reading_list_spec.rb

--- lib/reader.rb
+++ lib/reader.rb
@@ -4,6 +4,7 @@ require 'dotenv/load'
 require_relative 'reader/version'
 require_relative 'reader/book'
 require_relative 'reader/get_book'
+require_relative 'reader/reading_list'
 
 module Reader
 end
