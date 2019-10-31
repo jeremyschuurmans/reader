1793f3ca621d96b3d141548d4e0c170cbe8515f0
Jeremy Schuurmans <schuurm@ns.codes>
2019-10-30 18:39:44 -0700

    require net::http and json, make API call

--- lib/reader/get_book.rb
+++ lib/reader/get_book.rb
@@ -1,4 +1,14 @@
 class Reader::GetBook
+  require 'net/http'
+  require 'json'
+
+  def self.from_google_books(query)
+    url = "https://www.googleapis.com/books/v1/volumes?q=#{query}&maxResults=5&orderBy=relevance&printType=books&key=#{ENV['GOOGLE_BOOKS_KEY']}"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+
+  end
+
 
 
 
