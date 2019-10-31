af3008e9f2d41559cc6eee37929bdb3740641986
Jeremy Schuurmans <schuurm@ns.codes>
2019-10-31 12:57:43 -0700

    finish get_books method, add method displaying books retreived from the API

--- lib/reader/cli.rb
+++ lib/reader/cli.rb
@@ -13,7 +13,18 @@ class Reader::CLI
   def get_books
     user_input = gets.chomp
     Reader::GetBook.from_google_books(user_input)
+    display_books
   end
 
+  def display_books
+    books = Reader::Book.all
+
+    books.each.with_index(1) do |book, index|
+      puts "#{index} - #{book.title}"
+      puts "#{book.subtitle}" if book.subtitle != nil
+      puts "#{book.author}"
+      puts "#{book.publisher}"
+    end
+  end
 
 end
