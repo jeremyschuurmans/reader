79c4409943c229c0ecd649dede8a663a540dab06
Jeremy Schuurmans <schuurm@ns.codes>
2019-11-03 09:31:24 -0800

    make comment multiline

--- spec/reader/reading_list_spec.rb
+++ spec/reader/reading_list_spec.rb
@@ -38,5 +38,10 @@ RSpec.describe Reader::ReadingList do
 end
 
 =begin 
-These tests may need to be altered with data from spec/fixtures/vcr_cassettes/reader.yml if spec/fixtures is deleted, or VCR generates a new cassette (for example, the first time you run the tests after downloading this program). If the body in reader.yml is not readable, you can find the return values of the methods in this test by running bin/console, then Reader::GetBook.from_google_books('ruby') or whatever search term you'd like to use for these tests, then Reader::Book.all.last, and Reader::Book.all.first
+These tests may need to be altered with data from spec/fixtures/vcr_cassettes/reader.yml
+if spec/fixtures is deleted, or VCR generates a new cassette (for example, the first 
+time you run the tests after downloading this program). If the body in reader.yml is 
+not readable, you can find the return values of the methods in this test by running 
+bin/console, then Reader::GetBook.from_google_books('ruby') or whatever search term you'd 
+like to use for these tests, then Reader::Book.all.last, and Reader::Book.all.first
 =end
