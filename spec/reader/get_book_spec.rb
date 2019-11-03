b7703a4e8811d18e2ba29d78b9ef271eb416107b
Jeremy Schuurmans <schuurm@ns.codes>
2019-11-03 09:27:52 -0800

    change comments to be more informative

--- spec/reader/get_book_spec.rb
+++ spec/reader/get_book_spec.rb
@@ -2,13 +2,12 @@ require 'spec_helper'
 require 'pry'
 
 RSpec.describe Reader::GetBook do
-  # These tests may need to altered with data from spec/fixtures/vcr_cassettes/reader.yml if VCR creates a new cassette, or spec/fixtures is deleted.
   describe '.from_google_books' do
     it 'can fetch and parse books data from the Google Books API' do
      VCR.use_cassette("reader") do
        response = Reader::GetBook.from_google_books('ruby')
        expect(response).to be_a(Array)  
-       # When the method parses the json it's a hash, but then it sets it to a variable (books) and iterates over books['items'] which is an Array. 
+       # When the method parses the json it's a hash, but then it sets it to a variable (books) and iterates over books['items'] which is an Array, so the return value of response is an array in this method.
       end
     end
 
@@ -22,3 +21,7 @@ RSpec.describe Reader::GetBook do
     end
   end
 end
+
+=begin 
+These tests may need to be altered with data from spec/fixtures/vcr_cassettes/reader.yml if spec/fixtures is deleted, or VCR generates a new cassette (for example, the first time you run the tests after downloading this program). If the body in reader.yml is not readable, you can find the return values of the methods in this test by running bin/console, then Reader::GetBook.from_google_books('ruby') or whatever search term you'd like to use for these tests, then Reader::Book.all.last, and Reader::Book.all.first
+=end
