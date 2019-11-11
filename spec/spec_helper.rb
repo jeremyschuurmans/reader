e3059bc875259ea9b7bd351684293c9f15ec9b46
Jeremy Schuurmans <schuurm@ns.codes>
2019-11-11 09:55:46 -0800

    configure VCR to hide API key

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require "vcr"
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
+  config.filter_sensitive_data("<GOOGLE_BOOKS_API_KEY>") { ENV['GOOGLE_BOOKS_KEY'] }
 end
 
 RSpec.configure do |config|
