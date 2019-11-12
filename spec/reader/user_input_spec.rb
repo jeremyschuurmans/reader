c792d50f0c448704a373fdff1613d80f09fc10b1
Jeremy Schuurmans <schuurm@ns.codes>
2019-11-11 16:22:37 -0800

    add test for converting 1-5 strings to integers

--- spec/reader/user_input_spec.rb
+++ spec/reader/user_input_spec.rb
@@ -8,7 +8,17 @@ RSpec.describe Reader::UserInput do
 
       user_input = Reader::UserInput.new(input)
 
-      expect(user_input.input_from_user).to eql('ruby')
+      expect(user_input.input_from_user).to eq('ruby')
+    end
+  end
+
+  describe '#convert_input' do
+    it "converts strings '1' through '5' to integers and substracts 1" do
+      input = StringIO.new("1\n")
+
+      user_input = Reader::UserInput.new(input)
+
+      expect(user_input.convert_input).to eq(1)
     end
   end
 end
