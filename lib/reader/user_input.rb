fb715fe09e34a8d2a0e0980ec5ac2382b8ffad28
Jeremy Schuurmans <schuurm@ns.codes>
2019-11-12 12:49:59 -0800

    update tests and combine methods into one in user_input.rb (fixes bugs)

--- lib/reader/user_input.rb
+++ lib/reader/user_input.rb
@@ -5,14 +5,12 @@ class Reader::UserInput
     @input = input
   end
       
-  def input
+  def get_input
     @input = @input.gets.chomp.downcase
-  end
-      
-  def input_to_integer
+
     if @input.length == 1 && SELECTIONS.include?(@input)
       @input = @input.to_i-1
     end
     @input
-  end 
+  end
 end
--- spec/reader/user_input_spec.rb
+++ spec/reader/user_input_spec.rb
@@ -2,17 +2,15 @@ require 'spec_helper'
 require 'pry'
 
 RSpec.describe Reader::UserInput do
-  describe '#input' do
+  describe '#get_input' do
     it 'receives input from user and returns it chomped and downcased' do
       mock_input = StringIO.new("Ruby\n")
 
       user_input = Reader::UserInput.new(input: mock_input)
 
-      expect(user_input.input).to eq('ruby')
+      expect(user_input.get_input).to eq('ruby')
     end
-  end
 
-  describe '#input_to_integer' do
     it "converts strings '1' through '5' to integers and substracts 1, returns all other input as a string" do
       mock_number_one     = StringIO.new("1")
       mock_number_five    = StringIO.new("5")
@@ -24,15 +22,10 @@ RSpec.describe Reader::UserInput do
       user_input_six  = Reader::UserInput.new(input: mock_number_six)
       user_input_nan  = Reader::UserInput.new(input: mock_not_a_number)
 
-      user_input_one.input
-      user_input_five.input
-      user_input_six.input
-      user_input_nan.input
-
-      expect(user_input_one.input_to_integer).to eq(0)
-      expect(user_input_five.input_to_integer).to eq(4)
-      expect(user_input_six.input_to_integer).to eq("6")
-      expect(user_input_nan.input_to_integer).to eq("cherry pie")
+      expect(user_input_one.get_input).to eq(0)
+      expect(user_input_five.get_input).to eq(4)
+      expect(user_input_six.get_input).to eq("6")
+      expect(user_input_nan.get_input).to eq("cherry pie")
     end
   end
 end
