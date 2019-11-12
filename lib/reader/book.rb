831c56e628ce75db56bb61988919c3037bb99e3e
Jeremy Schuurmans <schuurm@ns.codes>
2019-11-12 13:47:38 -0800

    change attr_accessor to attr_reader, remove code dealing with managing book lists

--- lib/reader/book.rb
+++ lib/reader/book.rb
@@ -1,21 +1,10 @@
 class Reader::Book
-  attr_accessor :title, :subtitle, :author, :publisher
-
-  @@books = []
+  attr_reader :title, :subtitle, :author, :publisher
 
   def initialize(title, subtitle = nil, author, publisher)
     @title     = title
     @subtitle  = subtitle # defaults to nil in case some books don't have a subtitle.
     @author    = author
     @publisher = publisher
-    @@books << self
-  end
-
-  def self.all
-    @@books
-  end
-
-  def self.clear
-    @@books = []
   end
 end
