ca436f704aedc451dcf60f66c77f15880adb4c4c
Jeremy Schuurmans <schuurm@ns.codes>
2019-11-12 11:53:50 -0800

    finish user input tests and class

--- lib/reader/user_input.rb
+++ lib/reader/user_input.rb
@@ -1,19 +1,18 @@
 class Reader::UserInput
   SELECTIONS = ['1', '2', '3', '4', '5']
 
-  def initialize(input)
+  def initialize(input: $stdin)
     @input = input
   end
-
-  def input_from_user
-    @input.gets.chomp.downcase
+      
+  def input
+    @input = @input.gets.chomp.downcase
   end
-
-  def convert_input
+      
+  def input_to_integer
     if @input.length == 1 && SELECTIONS.include?(@input)
       @input = @input.to_i-1
     end
     @input
-  end
-
+  end 
 end
--- spec/reader/user_input_spec.rb
+++ spec/reader/user_input_spec.rb
@@ -2,26 +2,37 @@ require 'spec_helper'
 require 'pry'
 
 RSpec.describe Reader::UserInput do
-  describe '#input_from_user' do
+  describe '#input' do
     it 'receives input from user and returns it chomped and downcased' do
-      input = StringIO.new("Ruby\n")
+      mock_input = StringIO.new("Ruby\n")
 
-      user_input = Reader::UserInput.new(input)
+      user_input = Reader::UserInput.new(input: mock_input)
 
-      expect(user_input.input_from_user).to eq('ruby')
+      expect(user_input.input).to eq('ruby')
     end
   end
 
-  describe '#convert_input' do
-    it "converts strings '1' through '5' to integers and substracts 1" do
-      numerical_string     = "1"
-      numerical_string_too = "5"
+  describe '#input_to_integer' do
+    it "converts strings '1' through '5' to integers and substracts 1, returns all other input as a string" do
+      mock_number_one     = StringIO.new("1")
+      mock_number_five    = StringIO.new("5")
+      mock_number_six     = StringIO.new("6")
+      mock_not_a_number   = StringIO.new("cherry pie")
 
-      numerical_input = Reader::UserInput.new(numerical_string)
-      numerical_input_too = Reader::UserInput.new(numerical_string_too)
+      user_input_one  = Reader::UserInput.new(input: mock_number_one)
+      user_input_five = Reader::UserInput.new(input: mock_number_five)
+      user_input_six  = Reader::UserInput.new(input: mock_number_six)
+      user_input_nan  = Reader::UserInput.new(input: mock_not_a_number)
 
-      expect(numerical_input.convert_input).to eq(0)
-      expect(numerical_input_too.convert_input).to eq(4)
+      user_input_one.input
+      user_input_five.input
+      user_input_six.input
+      user_input_nan.input
+
+      expect(user_input_one.input_to_integer).to eq(0)
+      expect(user_input_five.input_to_integer).to eq(4)
+      expect(user_input_six.input_to_integer).to eq("6")
+      expect(user_input_nan.input_to_integer).to eq("cherry pie")
     end
   end
 end
