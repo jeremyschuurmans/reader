bcd534ed372c8019588e63a15859c01bf19b9526
Jeremy Schuurmans <schuurm@ns.codes>
2019-11-12 15:30:38 -0800

    expand reading list to handle creation and management of all lists

--- lib/reader/reading_list.rb
+++ lib/reader/reading_list.rb
@@ -1,11 +1,20 @@
-class Reader::ReadingList
-  @@list = []
+class Reader::ListCreator
+  @books        = []
+  @reading_list = []
+  
+  def self.create_list(books)
+    @books << books
+  end
+  
+  def self.add_to_reading_list(book)
+    @reading_list << book
+  end
 
-  def self.add_to_list(book)
-    @@list << book
+  def self.all_books
+    @books
   end
 
   def self.view_list
-    @@list
+    @reading_list
   end
 end
