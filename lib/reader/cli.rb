4671833496357f5ecbde20b4c5b9c33aee863a81
Jeremy Schuurmans <schuurm@ns.codes>
2019-11-12 20:13:04 -0800

    refactor cli, move some repeated code into methods, polish user input, cover edge cases

--- lib/reader/cli.rb
+++ lib/reader/cli.rb
@@ -1,6 +1,8 @@
 class Reader::CLI
   def initialize
+    puts ""
     puts "Welcome to Reader!"
+    puts ""
   end
 
   def call
@@ -14,14 +16,21 @@ class Reader::CLI
       case user_input
       when 'search'
         get_books
-      when user_input.is_a?(Integer) 
-        save_book
       when 'list'
         view_list
       when 'prev'
         display_books
+      else
+        unless user_input == 'exit'
+          puts ""
+          puts "Invalid input. Please try again."
+          puts ""
+
+          user_prompts
+        end
       end
     end
+    puts ""
     puts "Goodbye!"
   end
 
@@ -29,6 +38,7 @@ class Reader::CLI
     clear_search
 
     puts "Enter a word or phrase to find some books:"
+    puts ""
 
     user_input = Reader::UserInput.new.get_input
     
@@ -39,6 +49,8 @@ class Reader::CLI
 
   def display_books
     books = Reader::ListCreator.all_books
+
+    puts ""
 
     books.each.with_index(1) do |book, index|
       puts "#{index} - Title: #{book.title}"
@@ -52,17 +64,26 @@ class Reader::CLI
 
   def save_book
     puts "To save a book to your reading list, enter the corresponding number:"
-    index      = Reader::UserInput.new.get_input
-    book       = Reader::ListCreator.all_books[index]
 
-    Reader::ListCreator.add_to_reading_list(book)
+    index  = Reader::UserInput.new.get_input
 
-    puts "Saved!"
-    puts ""
-    puts "To view your previous search, enter 'prev'"
-    puts "To view your reading list, enter 'list'"
-    puts "To search again, enter 'search'"
-    puts "To exit, enter 'exit'"
+    if index.is_a?(Integer)
+      book = Reader::ListCreator.all_books[index]
+
+      Reader::ListCreator.add_to_reading_list(book)
+
+      puts ""
+      puts "Saved!"
+      puts ""
+
+      user_prompts
+    else
+      puts ""
+      puts "You must enter a number to save a book. Enter 'prev' to view your previous search and save books."
+      puts ""
+
+      user_prompts
+    end
   end
 
   def view_list
@@ -70,6 +91,7 @@ class Reader::CLI
     
     puts ""
     puts "MY READING LIST"
+    puts ""
 
     list.each.with_index(1) do |list_item, index|
       puts "#{index} - Title: #{list_item.title}"
@@ -77,10 +99,18 @@ class Reader::CLI
       puts "    Author: #{list_item.author}"
       puts "    Publisher: #{list_item.publisher}"
       puts ""
+      puts ""
+      puts ""
+      
+      user_prompts
     end
+  end
 
+  def user_prompts
     puts "To view your previous search, enter 'prev'"
+    puts "To view your reading list, enter 'list'"
     puts "To search again, enter 'search'"
+    puts "To exit, enter 'exit'"
   end
 
   def clear_search
